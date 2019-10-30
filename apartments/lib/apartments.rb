678fe1eada1fb341c527c0ccba36646633a17d49
Alina Kornilova <alinakornilova78@gmail.com>
2019-10-30 18:52:33 +0000

    modified file

--- apartments/lib/apartments.rb
+++
@@ -1,7 +0,0 @@
-require "open-uri"
-require "nokogiri"
-require "pry"
-
-require_relative "./apartments/version"
-require_relative './apartments/rentals'
-require_relative './apartments/cli'
--- apartments/lib/apartments/version.rb
+++
@@ -1,3 +0,0 @@
-module Apartments
-  VERSION = "0.1.0"
-end
